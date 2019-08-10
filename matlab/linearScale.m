ffb8c9119d055f7d75748c4e724d88f6c76b4349
Jason Conklin <j@ninly.net>
2019-08-09 20:36:33 -0500

    remove old dev comments

--- matlab/linearScale.m
+++ matlab/linearScale.m
@@ -1,10 +1,23 @@
 function [deg,I,mos,step] = linearScale(N, gen, per)
-%% build a linear scale
+% LINEARSCALE build a rank-2 linear scale and represent its degree
+%   values (and some associated data)
+%
+% Inputs
+%   N : number of desired scale degrees
+%   gen : generator value in cents (default: JI fifth)
+%   per : scale period in cents (default: 1200)
+%
+% Outputs
+%   deg : column vector of degree values in cents (skipping 1/1,
+%         and including period)
+%   I : generator order corresponding to each degree
+%   mos : boolean indicating whether scale is a moment of symmetry
+%   step : step size in cents for each degree
+%
+% TODO
+%   This does not allow for octave-repeating scales with a period less than 2/1.
+%   Is that a real thing?
 
-% per = 2/1; % period
-% gen = 3/2; % generator
-
-% N = 7;
 if ~exist('per','var')
     per = 1200;
 end
@@ -20,7 +33,7 @@ for idx = 1:N-1
     while nextDeg > per
         nextDeg = nextDeg-per;
     end
-    
+
     if ismember(nextDeg,deg) || nextDeg == per
         warning(['This generator cannot produce more than ' num2str(idx) ' scale degrees.']);
         deg(isnan(deg)) = []; % clear allocated spaces that won't be filled
@@ -34,15 +47,11 @@ if ~ismember(per,deg)
     deg = [deg;per];
 end
 
-% deg_cents = cents(deg_rat);
 [deg,I] = sort(deg);
 I = [I(1:end-1);0];
 
-% sorted_rat = sort(deg_rat);
-% step_ratios = [sorted_rat]./[1;sorted_rat(1:end-1)]; % BROKEN
-
 % determine whether scale is a moment of symmetry
-step = round(diff([0;deg]),10); % round to 10 decimal places
+step = round(diff([0;deg]), 10); % round to 10 decimal places
 mos = false;
 if length(unique(step)) == 2
     mos = true;
